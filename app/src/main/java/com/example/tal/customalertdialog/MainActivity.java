9563ac37f65f7e1ef4be3f386151b0a2d4b6b0f2
Tal <ty4273@bs.amalnet.k12.il>
2018-11-08 20:45:29 +0200

    Final commit

--- app/src/main/java/com/example/tal/customalertdialog/MainActivity.java
+++ app/src/main/java/com/example/tal/customalertdialog/MainActivity.java
@@ -2,17 +2,20 @@ package com.example.tal.customalertdialog;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     //items in activity_main.xml
@@ -20,10 +23,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     ListView lv1;
 
     LinearLayout dialog;
-    //items in dialog.xml
-    RadioGroup dialog_type;
-    RadioButton ari, geo;
-    EditText dialog_et_x1, dialog_et_d;
 
     //global variables
     int type;
@@ -42,12 +41,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         tv4 = findViewById(R.id.tv4);
         lv1 = findViewById(R.id.lv1);
         lv1.setOnItemClickListener(this);
-        //items on dialog.xml
-        dialog_type = findViewById(R.id.dialog_type);
-        ari = findViewById(R.id.ari);
-        geo = findViewById(R.id.geo);
-        dialog_et_x1 = findViewById(R.id.dialog_et_x1);
-        dialog_et_d = findViewById(R.id.dialog_et_d);
+
+        fillListView(R.id.ari, x1, d);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent t = new Intent(this, CreditsActivity.class);
+        if (id == R.id.MenuCredits)
+            startActivity(t);
+        return super.onOptionsItemSelected(item);
     }
 
     public void OCbtn1(View view) {
@@ -56,16 +66,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adb.setView(dialog);
         adb.setCancelable(false);
         adb.setTitle("Enter Data");
+
+        final RadioGroup dialog_type = dialog.findViewById(R.id.dialog_type);
+        final EditText dialog_et_x1 = dialog.findViewById(R.id.dialog_et_x1);
+        final EditText dialog_et_d = dialog.findViewById(R.id.dialog_et_d);
+
         adb.setPositiveButton("Enter", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                type = dialog_type.getCheckedRadioButtonId();
-                x1 = Integer.parseInt(dialog_et_x1.getText().toString());
-                d = Integer.parseInt(dialog_et_d.getText().toString());
-                fillListView(type, x1, d);
-                tv1.setText("X1= " + x1);
-                tv2.setText("d= " + d);
-                dialog.dismiss();
+                if (dialog_et_x1.getText().toString().equals("") || dialog_et_d.getText().toString().equals(""))
+                    showToast();
+                else {
+                    type = dialog_type.getCheckedRadioButtonId();
+                    x1 = Integer.parseInt(dialog_et_x1.getText().toString());
+                    d = Integer.parseInt(dialog_et_d.getText().toString());
+                    fillListView(type, x1, d);
+                    dialog.dismiss();
+                }
             }
         });
         adb.setNegativeButton("Reset", new DialogInterface.OnClickListener() {
@@ -74,8 +91,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 x1 = 0;
                 d = 0;
                 fillListView(type, x1, d);
-                tv1.setText("X1= " + x1);
-                tv2.setText("d= " + d);
                 dialog.dismiss();
             }
         });
@@ -88,8 +103,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adb.show();
     }
 
+    private void showToast() {
+        Toast.makeText(this, "Please fill all fields.", Toast.LENGTH_LONG).show();
+    }
+
     public void fillListView(int type, int x1, int d) {
-        list[1] = x1;
+        list[0] = x1;
         if (type == R.id.ari)
             for (int i = 1; i < 20; i++)
                 list[i] = list[i - 1] + d;
@@ -100,6 +119,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             listString[i] = String.valueOf(list[i]);
         ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, listString);
         lv1.setAdapter(adp);
+        tv1.setText("X1= " + x1);
+        tv2.setText("d= " + d);
+        tv3.setText("n= ");
+        tv4.setText("Sn= ");
     }
 
     @Override
